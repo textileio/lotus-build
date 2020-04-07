1e4e38811f53d0f694d010150adab404b2999e9a
Ignacio Hagopian <jsign.uy@gmail.com>
2020-04-07 09:57:56 -0300

    first ver
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+revproxy &
+lotus daemon
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -rf lotus
+git clone --depth=1 https://github.com/filecoin-project/lotus.git -b master
+cd lotus
+
+TAG=$(git rev-parse --short HEAD)
+if docker pull textile/lotus:$TAG > /dev/null; then
+  echo "Docker image of $TAG already exists, nothing to do."
+else
+  echo "Building image..."
+fi
+
+make deps
+cd ..
+
+docker build -t textile/lotus:$TAG -t textile/lotus:latest .
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+docker push textile/lotus:$TAG
+docker push textile/lotus:latest
