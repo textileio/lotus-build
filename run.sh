244a2febbe809368360e8946f4d1c90b6fdb82ec
Ignacio Hagopian <jsign.uy@gmail.com>
2020-06-19 10:15:17 -0300

    include versioned tags
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ if docker pull textile/lotus:$TAG > /dev/null; then
 else
   echo "Building image..."
 fi
+TAG_VERSIONED=$(git describe --exact-match HEAD)
 
 make deps
 cd ..
@@ -19,3 +20,8 @@ docker build -t textile/lotus:$TAG -t textile/lotus:latest .
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 docker push textile/lotus:$TAG
 docker push textile/lotus:latest
+
+if [[!-z "$TAG_VERSIONED"]]
+then
+  docker push textile/lotus:$TAG_VERSIONED
+fi
