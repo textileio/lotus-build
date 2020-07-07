7908194220a9e7c17ba34665e79ea4a3f439a992
Ignacio Hagopian <jsign.uy@gmail.com>
2020-07-07 20:49:24 -0300

    config for nerpa
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 rm -rf lotus
-git clone --depth=1 https://github.com/filecoin-project/lotus.git -b master
+git clone --depth=1 https://github.com/filecoin-project/lotus.git -b ntwk-nerpa
 cd lotus
 
 TAG=$(git rev-parse --short HEAD)
-if docker pull textile/lotus:$TAG > /dev/null; then
+if docker pull textile/lotus:nerpa-$TAG > /dev/null; then
   echo "Docker image of $TAG already exists, nothing to do."
   exit 0
 else
@@ -16,14 +16,13 @@ TAG_VERSIONED=$(git describe --exact-match HEAD)
 make deps
 cd ..
 
-docker build -t textile/lotus:$TAG -t textile/lotus:latest .
+docker build -t textile/lotus:nerpa-$TAG .
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
-docker push textile/lotus:$TAG
-docker push textile/lotus:latest
+docker push textile/lotus:nerpa-$TAG
 
 echo $TAG_VERSIONED
 if [ -n "$TAG_VERSIONED" ]
 then
-  docker build -t textile/lotus:$TAG_VERSIONED .
-  docker push textile/lotus:$TAG_VERSIONED
+  docker build -t textile/lotus:nerpa-$TAG_VERSIONED .
+  docker push textile/lotus:nerpa-$TAG_VERSIONED
 fi
