cd25cc01f059210bbc839e16876f14a315956549
Ignacio Hagopian <jsign.uy@gmail.com>
2020-08-17 18:44:05 -0300

    touch to fire action
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf lotus
-git clone --depth=1 https://github.com/filecoin-project/lotus.git -b ntwk-nerpa
+git clone --depth=1 https://github.com/filecoin-project/lotus.git -b ntwk-nerpa 
 cd lotus
 
 TAG=$(git rev-parse --short HEAD)
